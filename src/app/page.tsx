71028f95fbb7e7d7f9179b6a3bc591e558706d2f
DevBazz <bazaniqbal@gmail.com>
2026-01-08 22:54:54 +0500

    Responsive Navbar added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,11 @@
-import ToggleTheme from '@/components/toggle-theme'
-import { Button } from '@/components/ui/button'
-import { SignedIn, SignedOut, SignInButton, SignUpButton, UserButton } from '@clerk/nextjs'
+
 import React from 'react'
 
 const HomePage = () => {
   return (
-    <>
-    <SignedOut>
-              <SignInButton mode='modal'>
-                <Button>Sign In</Button>
-              </SignInButton>
-              <SignUpButton mode='modal'>
-                <Button variant='outline' className='ml-2'>Sign Up</Button>
-              </SignUpButton>
-            </SignedOut>
-            <SignedIn>
-              <UserButton />
-            </SignedIn>
-
-            <ToggleTheme/>
-    </>
+    <div className='m-4'>
+      <h1>Home Content</h1>
+    </div>
   )
 }
 
