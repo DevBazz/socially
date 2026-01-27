5e7245e241bc62a24072a49c975513ff5dedd40a
DevBazz <bazaniqbal@gmail.com>
2026-01-27 21:35:48 +0500

    Api Route Handler

---
+++ src/app/tasks/page.tsx
@@ -0,0 +1,14 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+
+
+const Tasks = async () => {
+
+ const response = await fetch('http://localhost:3000/api/tasks')
+ const data = await response.json()
+  
+  return (
+    <div>Tasks</div>
+  )
+}
+
+export default Tasks
